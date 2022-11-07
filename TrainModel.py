e32f5fe556440108bf4cd12888e0633144e1691e
Jakub Konert <83219666+JakubKonert@users.noreply.github.com>
2022-11-07 23:10:58 +0100

    ModelFinder

--- TrainModel.py
+++ TrainModel.py
@@ -13,7 +13,7 @@ import autokeras as ak
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0],True)
 
-batch_size = 32
+batch_size = 8
 img_height = 480
 img_width = 640
 
@@ -30,21 +30,21 @@ trainDS = tf.keras.utils.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
-# validDS = tf.keras.utils.image_dataset_from_directory(
-#   trainDSPath,
-#   validation_split=0.2,
-#   subset="validation",
-#   seed=123,
-#   image_size=(img_height, img_width),
-#   batch_size=batch_size)
+validDS = tf.keras.utils.image_dataset_from_directory(
+  trainDSPath,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
 
-# testDS = tf.keras.utils.image_dataset_from_directory(
-#   testDSPath,
-#   validation_split=0.2,
-#   subset="training",
-#   seed=123,
-#   image_size=(img_height, img_width),
-#   batch_size=batch_size)
+testDS = tf.keras.utils.image_dataset_from_directory(
+  testDSPath,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
 
 classNames = trainDS.class_names
 
@@ -53,39 +53,38 @@ clf = ak.ImageClassifier(
     max_trials=1
 )
 
-print(trainDS)
 
 history = clf.fit(trainDS)
 model=clf.export_model()
 model.summary()
 
 
-# AUTOTUNE = tf.data.AUTOTUNE
-# trainDS = trainDS.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-# validDS = validDS.cache().prefetch(buffer_size=AUTOTUNE)
-# testDS = testDS.cache().prefetch(buffer_size=AUTOTUNE)
+AUTOTUNE = tf.data.AUTOTUNE
+trainDS = trainDS.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+validDS = validDS.cache().prefetch(buffer_size=AUTOTUNE)
+testDS = testDS.cache().prefetch(buffer_size=AUTOTUNE)
 
-# normalization_layer = layers.Rescaling(1./255)
-# normalizedDS = trainDS.map(lambda x, y: (normalization_layer(x), y))
-# image_batch, labels_batch = next(iter(normalizedDS))
+normalization_layer = layers.Rescaling(1./255)
+normalizedDS = trainDS.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalizedDS))
 
-# numClasses = len(classNames)
+numClasses = len(classNames)
 
-# model = Sequential([
-#   layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-#   layers.Conv2D(16, 3, padding='same', activation='relu'),
-#   layers.MaxPooling2D(),
-#   layers.Conv2D(32, 3, padding='same', activation='relu'),
-#   layers.MaxPooling2D(),
-#   layers.Conv2D(64, 3, padding='same', activation='relu'),
-#   layers.MaxPooling2D(),
-#   layers.Flatten(),
-#   layers.Dense(128, activation='relu'),
-#   layers.Dense(numClasses)
-# ])
+model = Sequential([
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(numClasses)
+])
 
-# model.compile(optimizer='adam',
-#               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-#               metrics=['accuracy'])
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
-# model.summary()
+model.summary()
